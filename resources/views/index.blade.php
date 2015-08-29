432a58e7d310438ac330fd39a69b322a66d8fd6c
luke <luke@HsuLukedeMacBook-Pro.local>
2015-08-29 16:54:16 +0800

    good

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
   <meta name="_token" content="{{ csrf_token() }}"/>
   <title>登入頁面</title>
   <link rel="stylesheet"  href="./bootstrap331/dist/css/bootstrap.css">
@@ -12,26 +13,24 @@
 <body>
   <div class="container-fluid">
     <div class="row">
-      <div class="login-form col-md-4 col-md-offset-4" style="height:150px;background-color:#FFFFFF" >
-
-      </div>
-    </div>
-    <div class="row">
-      <div class="login-form col-md-4 col-md-offset-4" style="Z-index:9999;" id="logindisplay" >
+      <div class="login-form col-md-4  col-md-offset-4"  style="height:350px;display:none;margin-top:60px;" id="logindisplay" >
+        <h4>Bora</h4>
         <div class="form-group">
-          <input type="text" class="form-control login-field" value="" placeholder="Account" id="login-name" autocomplete="on" />
+          <input type="text" class="form-control login-field" style="margin-top:20px;" value="" placeholder="Account" id="login-name" autocomplete="on" />
           <label class="login-field-icon fui-user" for="login-name"></label>
         </div>
         <div class="form-group">
           <input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" />
           <label class="login-field-icon fui-lock" for="login-pass"></label>
         </div>
-        <a class="btn btn-primary btn-lg btn-block" id="checklogin">Login</a>
+        <label class="" id="error" >&nbsp</label>
+        <a class="btn btn-primary btn-lg btn-block" id="checklogin" style="margin-top:25px;">Login</a>
       </div>
     </div>
   </div> 
 <script type="text/javascript">
     $(document).ready(function() {
+      $('#logindisplay').fadeIn(1000);
       $("#checklogin").click(function()
       {  
         $.ajax({
@@ -44,7 +43,7 @@
           window.location.replace("http://127.0.0.1/eip/public/dashboard" );
         },
         error: function(xhr, type){
-          alert('Ajax error!')
+          $('#error').html('您所輸入的帳號或密碼錯誤')
         }
       }); 
     });  
